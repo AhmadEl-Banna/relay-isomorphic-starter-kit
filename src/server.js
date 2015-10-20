aa0b020dbab0742db6eb7724f05a32ab75b6ccdb
DanielHuisman <danielhuisman.development@gmail.com>
2015-10-20 18:38:33 +0200

    Added the ability to turn off isomorphic rendering (aka client side app)

--- src/server.js
+++ src/server.js
@@ -24,7 +24,9 @@ const config = {
 	https: process.env.HTTPS === true || process.env.HTTPS === "true",
 	httpsOptions: {},
 
-	distURL: process.env.DIST_URL || ""
+	distURL: process.env.DIST_URL || "",
+
+	isomorphic: process.env.ISOMORPHIC === true || process.env.ISOMORPHIC === "true"
 };
 
 const log = new winston.Logger({
@@ -72,62 +74,64 @@ app.use("/graphql", expressGraphQL({
 }));
 
 app.use((req, res, next) => {
-	let render = [
-		`<!DOCTYPE html>
-		<html>
-			<head>
-				<title>relay-isomorphic-starter-kit</title>
-				<meta charset="utf-8" />
-			</head>
-			<body>
-				<div id="react-root"></div>
-				<script type="text/javascript" src="${config.distURL}/dist/client.js"></script>
-			</body>
-		</html>`
-	].join("");
+	// Isomorphic rendering is disabled until it actually works
+	if(false || config.isomorphic) {
+		// let location = createLocation(req.originalUrl);
 
-	res.type("text/html");
-	res.send(render);
+		// TODO: fix this after Relay server-side rendering gets fixed
 
+		// match({routes, location}, (err, redirectionLocation, renderProps) => {
+		// 	if(redirectionLocation) {
+		// 		return res.redirect(redirectionLocation.pathname);
+		// 	}
+		// 	if(err) {
+		// 		return next(err);
+		// 	}
+		// 	if(!renderProps) {
+		// 		return next();
+		// 	}
 
-	// let location = createLocation(req.originalUrl);
+		// 	let rendered = ReactDOM.renderToString(
+		// 		<RoutingContext {...renderProps} createElement={ReactRouterRelay.createElement} />
+		// 	);
+		// 	let helmet = Helmet.rewind();
+		// 	let html = [
+		// 		`<!DOCTYPE html>,
+		// 		<html>
+		// 			<head>
+		// 				<title>${helmet.title}</title>`,
+		// 				helmet.meta,
+		// 				helmet.link,
+		// 				`<meta charset="utf-8" />
+		// 			</head>
+		// 			<body>
+		// 				<div id="react-root">${rendered}</div>
+		// 				<script type="text/javascript" src="/dist/client.js">
+		// 			</body>
+		// 		</html>`
+		// 	].join("");
+		//
+		// 	res.type("text/html");
+		// 	res.send(html);
+		// });
+	} else {
+		let render = [
+			`<!DOCTYPE html>
+			<html>
+				<head>
+					<title>relay-isomorphic-starter-kit</title>
+					<meta charset="utf-8" />
+				</head>
+				<body>
+					<div id="react-root"></div>
+					<script type="text/javascript" src="${config.distURL}/dist/client.js"></script>
+				</body>
+			</html>`
+		].join("");
 
-	// TODO: fix this after Relay server-side rendering gets fixed
-
-	// match({routes, location}, (err, redirectionLocation, renderProps) => {
-	// 	if(redirectionLocation) {
-	// 		return res.redirect(redirectionLocation.pathname);
-	// 	}
-	// 	if(err) {
-	// 		return next(err);
-	// 	}
-	// 	if(!renderProps) {
-	// 		return next();
-	// 	}
-
-	// 	let rendered = ReactDOM.renderToString(
-	// 		<RoutingContext {...renderProps} createElement={ReactRouterRelay.createElement} />
-	// 	);
-	// 	let helmet = Helmet.rewind();
-	// 	let html = [
-	// 		`<!DOCTYPE html>,
-	// 		<html>
-	// 			<head>
-	// 				<title>${helmet.title}</title>`,
-	// 				helmet.meta,
-	// 				helmet.link,
-	// 				`<meta charset="utf-8" />
-	// 			</head>
-	// 			<body>
-	// 				<div id="react-root">${rendered}</div>
-	// 				<script type="text/javascript" src="/dist/client.js">
-	// 			</body>
-	// 		</html>`
-	// 	].join("");
-	//
-	// 	res.type("text/html");
-	// 	res.send(html);
-	// });
+		res.type("text/html");
+		res.send(render);
+	}
 });
 
 app.use(expressWinston.errorLogger({
