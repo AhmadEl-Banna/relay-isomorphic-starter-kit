f086d774124e79cec667e6b8a726ddd32e2d504f
Daniel Huisman <danielhuisman.development@gmail.com>
2015-10-20 22:26:32 +0200

    Added some links to make the test page clearer

--- src/views/App.js
+++ src/views/App.js
@@ -18,6 +18,13 @@ class App extends React.Component {
 				</div>
 
 				<footer>
+					<ul>
+						<li><a href="/users/1">User 1</a></li>
+						<li><a href="/users/2">User 2</a></li>
+						<li><a href="/users/3">User 3</a></li>
+						<li><a href="/users/4">User 4</a></li>
+						<li><a href="/users/5">User 5</a></li>
+					</ul>
 					<p>
 						<a href="https://github.com/DanielHuisman/relay-isomorphic-starter-kit">
 							https://github.com/DanielHuisman/relay-isomorphic-starter-kit
