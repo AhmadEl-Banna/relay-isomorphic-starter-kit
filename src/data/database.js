e3c7473cc9db397ff44c958c41cfa359a6da6263
Daniel Huisman <danielhuisman.development@gmail.com>
2015-10-21 20:52:19 +0200

    Removed comment

--- src/data/database.js
+++ src/data/database.js
@@ -1,6 +1,5 @@
 import Sequelize from "sequelize";
 
-//TODO: change database configuration and set up migrations folder
 export default new Sequelize(null, null, null, {
 	dialect: "sqlite",
 	storage: ":memory:"
